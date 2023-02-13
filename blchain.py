14ae0f12bc226f3f423284cd907dd0bb54993e4e
kiranpranay <pranaybannu12@gmail.com>
2023-02-13 13:19:59 +0530

    Add Unique Block Id for every Unique User
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- blchain.py
+++ blchain.py
@@ -1,5 +1,6 @@
 import datetime
 import hashlib
+import uuid
 from flask import Flask, request, render_template
 
 class Block:
@@ -10,10 +11,12 @@ class Block:
 	nonce = 0
 	previous_hash = 0*0
 	timestamp = datetime.datetime.now()
+	block_id = None
 	
 	def __init__(self, data, name):
 		self.data = data
 		self.blockName = name
+		self.block_id = uuid.uuid1()
 		
 	def hash(self):
 		h = hashlib.sha256()
@@ -42,13 +45,19 @@ class Blockchain:
 		block.previous_hash = self.block.hash() 
 		self.block.next = block
 		self.block = self.block.next
+  
+	block_ids = {}
 	
 	def mine(self, block):
+		if block.blockName in self.block_ids:
+			block.block_id = self.block_ids[block.blockName]
+		else:
+			self.block_ids[block.blockName] = block.block_id
 		for n in range(self.maxNonce):
 			if int(block.hash(), 16) <= self.target:
 				self.add(block)
 				print(block)
-				return block.blockName,block.data
+				return block.blockName,block.data,block.block_id
 			else:
 				block.nonce += 1
 	
@@ -66,8 +75,8 @@ def my_form_post():
 	PatientName = request.form['PatientName']
 	Details = request.form['Details']
 	
-	nm, tx = blockchain.mine(Block(Details, PatientName))
-	my_dict.append([nm,tx])
+	nm, tx, id = blockchain.mine(Block(Details, PatientName))
+	my_dict.append([nm,tx,id])
 	return render_template('index.html', name_list = my_dict)
 
 if __name__=="__main__":
